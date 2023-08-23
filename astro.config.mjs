8841914bf4560f419a76839afb310708c1aabf7a
Cecilie Hansen Rørås <cecilie.roras@origo.oslo.kommune.no>
2023-08-23 14:25:28 +0200

    image upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@pkt": ["@oslokommune/punkt-css/dist"],
+        "@pkt-assets": ["@oslokommune/punkt-assets/dist"],
+        "~/*": ["./*"],
+        "@/*": ["./*"],
+        "~~/*": ["./*"],
+        "@@/*": ["./*"]
+      }
+    }
+  },
+  integrations: [react(), vue()]
+});
---
+++ src/components/ImageUpload.tsx
@@ -0,0 +1,58 @@
+import React, { useState, FormEvent } from "react"; // Import FormEvent type
+import { PktButton, PktTextinput } from "@oslokommune/punkt-react";
+
+export default function ImageUpload() {
+  const [responseMessage, setResponseMessage] = useState("");
+  const [hasError, setHasError] = useState(false);
+
+  async function submit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    // Validate if the uploaded file is an image with specific formats
+    const file = formData.get("InputImage");
+    if (file instanceof File) {
+      const allowedFormats = ["image/jpeg", "image/png", "image/gif"];
+      if (!allowedFormats.includes(file.type)) {
+        setHasError(true);
+        return;
+      }
+    } else {
+      setHasError(false);
+    }
+
+    const response = await fetch("https://274m92bcfh.execute-api.eu-west-1.amazonaws.com/dev/", {
+      method: "POST",
+      body: formData,
+      headers: {
+        "X-API-Key": `${import.meta.env.API_KEY}`, // Replace with your actual API key
+      },
+    });
+
+    const data = await response.json();
+    if (data.message) {
+      setResponseMessage(data.message);
+    }
+  }
+
+  return (
+    <form className="pkt-form pkt-container--center" onSubmit={submit}>
+      <fieldset className="pkt-fieldset">
+        <legend>SnapMeal</legend>
+        <PktTextinput
+          id="InputImage"
+          label="Legg til et bilde"
+          hasError={hasError}
+          errorMessage="Please upload a valid image (JPEG, PNG, GIF)"
+          type="file"
+          accept=".jpeg, .jpg, .png, .gif" // Specify accepted file formats
+        />
+        <PktButton className="mt-size-16" skin="primary" variant="label-only" type="submit">
+          Snap en oppskrift
+        </PktButton>
+      </fieldset>
+
+      {responseMessage && <p>{responseMessage}</p>}
+    </form>
+  );
+}
